7098454a1846fc83bad96bedfb9ce10aa7209acd
Renildo Rabi <renildorabi22@gmail.com>
2022-04-29 23:08:59 -0300

    finalizado

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django.forms import ModelForm
+from app.models import Carros
+
+class CarrosForm(ModelForm):
+    class Meta:
+        model = Carros
+        fields = ['modelo','marca', 'ano']
